04c3f8955ef6d7285792cc205585b01fcdf100e5
Shuji Honda <honpya@gmail.com>
2017-05-10 22:09:50 +0900

    Customized for Taketodon

--- app/javascript/mastodon/components/display_name.js
+++ app/javascript/mastodon/components/display_name.js
@@ -8,6 +8,47 @@ export default class DisplayName extends React.PureComponent {
     others: ImmutablePropTypes.list,
   };
 
+  instanceClassName (domain) {
+    if (domain === '') {
+        return 'display-name';
+    }
+
+    let instanceClassName = '';
+    switch(domain) {
+      case 'biwakodon.com':
+        instanceClassName = 'biwako';
+        break;
+      case 'gingadon.com':
+        instanceClassName = 'ginga';
+        break;
+      case 'mastodon.nara.jp':
+        instanceClassName = 'shika';
+        break;
+      case 'mastodon.wakayama.jp':
+        instanceClassName = 'wakayama';
+        break;
+      case 'mastodos.com':
+        instanceClassName = 'kyoto';
+        break;
+      case 'miestodon.com':
+        instanceClassName = 'mie';
+        break;
+      case 'nagoyadon.jp':
+        instanceClassName = 'nagoya';
+        break;
+      case 'mstdn.hyogo.jp':
+        instanceClassName = 'hyogo';
+        break;
+      case 'mstdn.osaka':
+        instanceClassName = 'osaka';
+        break;
+      default:
+        instanceClassName = 'unknown';
+        break;
+    }
+    return `display-name ${instanceClassName}`;
+  }
+
   render () {
     const { account, others } = this.props;
     const displayNameHtml = { __html: account.get('display_name_html') };
@@ -20,8 +61,11 @@ export default class DisplayName extends React.PureComponent {
       suffix = <span className='display-name__account'>@{account.get('acct')}</span>;
     }
 
+    const matched = this.props.account.get('acct').match(/^(.*)@(.*)$/);
+    const domain = matched !== null ? matched[matched.length -1] : '';
+
     return (
-      <span className='display-name'>
+      <span className={this.instanceClassName(domain)}>
         <bdi><strong className='display-name__html' dangerouslySetInnerHTML={displayNameHtml} /></bdi> {suffix}
       </span>
     );
