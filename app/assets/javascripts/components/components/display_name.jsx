c02215d2491dcf05ffbc9661b33ec19ec25bd730
Shuji Honda <honpya@gmail.com>
2017-05-23 12:35:03 +0900

    Identify instances

--- app/assets/javascripts/components/components/display_name.jsx
+++ app/assets/javascripts/components/components/display_name.jsx
@@ -4,12 +4,24 @@ import emojify from '../emoji';
 
 class DisplayName extends React.PureComponent {
 
+  instanceClassName (domain) {
+    switch(domain) {
+      case 'mastodon.nara.jp':
+        return 'display-name shika';
+      default:
+        return 'display-name';
+    }
+  }
+
   render () {
     const displayName     = this.props.account.get('display_name').length === 0 ? this.props.account.get('username') : this.props.account.get('display_name');
     const displayNameHTML = { __html: emojify(escapeTextContentForBrowser(displayName)) };
 
+    const matched = this.props.account.get('acct').match(/^(.*)@(.*)$/);
+    const domain = matched !== null ? matched[matched.length -1] : null;
+
     return (
-      <span className='display-name'>
+      <span className={this.instanceClassName(domain)}>
         <strong className='display-name__html' dangerouslySetInnerHTML={displayNameHTML} /> <span className='display-name__account'>@{this.props.account.get('acct')}</span>
       </span>
     );
